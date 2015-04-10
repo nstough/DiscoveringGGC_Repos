89e6d44945901cde1cc00120bdcb5a358bdedae2
Nick <Nick@10.251.13.198>
2015-04-10 12:43:11 -0400

    4/10/15 added Game

---
+++ DiscoveringGGC/src/Game.java
@@ -0,0 +1,97 @@
+/**Class: Game
+ * @author Nick Stough
+ * @version 1.0
+ * Course : ITEC 2150 Fall 2013
+ * Written: Apr 7, 2015
+ *
+ * This class describe
+ *
+ * Purpose: This purpose is ....
+ */
+
+/**
+ * @author Nick
+ *
+ */
+public class Game
+{
+	public String b1Results;
+	public String b2Results;
+	public String b3Results;
+	public String b4Results;
+	
+	public Game(){
+		this.b1Results = "";
+		this.b2Results = "";
+		this.b3Results = "";
+		this.b4Results = "";
+	}
+
+	/**
+	 * @return the b1Results
+	 */
+	public String getB1Results()
+	{
+		return b1Results;
+	}
+
+	/**
+	 * @return the b2Results
+	 */
+	public String getB2Results()
+	{
+		return b2Results;
+	}
+
+	/**
+	 * @return the b3Results
+	 */
+	public String getB3Results()
+	{
+		return b3Results;
+	}
+
+	/**
+	 * @return the b4Results
+	 */
+	public String getB4Results()
+	{
+		return b4Results;
+	}
+
+	/**
+	 * @param b1Results the b1Results to set
+	 */
+	public void setB1Results(String b1Results)
+	{
+		this.b1Results = b1Results;
+	}
+
+	/**
+	 * @param b2Results the b2Results to set
+	 */
+	public void setB2Results(String b2Results)
+	{
+		this.b2Results = b2Results;
+	}
+
+	/**
+	 * @param b3Results the b3Results to set
+	 */
+	public void setB3Results(String b3Results)
+	{
+		this.b3Results = b3Results;
+	}
+
+	/**
+	 * @param b4Results the b4Results to set
+	 */
+	public void setB4Results(String b4Results)
+	{
+		this.b4Results = b4Results;
+	}
+	
+	
+	
+	
+}
