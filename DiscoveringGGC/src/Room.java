6cf031fde3151acf25790e0d1109dd4a6ea20d92
John Holcombe <jholcombe@ggc.edu>
2015-04-10 14:47:21 -0400

    Teacher and room heavily updated

--- DiscoveringGGC/src/Room.java
+++ DiscoveringGGC/src/Room.java
@@ -3,6 +3,8 @@
  * Description: Rooms can have teachers, items, or puzzles. Rooms will be real locations at GGC
  * Author: John Holcombe 
  */
+import java.util.Random; //needed to handle the random chance of encountering a teacher.
+
 public class Room 
 {
 	//Ryan was here
@@ -10,11 +12,37 @@ public class Room
 	int roomID; //This is the room id so that we know which room the player is in.
 	int[] puzzles; //This is a list of possible puzzles in this room
 	int[] teachers; // this is a list of possible teachers
-	int[] ajacentRooms; //This is an array of rooms that the player may enter from this room
+	int[] adjacentRooms; //This is an array of rooms that the player may enter from this room
+	Random random = new Random();
 	
-	public String getRoomDescription()
+	public String getRoomDescription() //method allowing the the room description to be given to the player
 	{
 		return roomDes;
+	}
+	
+	public boolean encounterTeacher(int chance) //chance to encounter the teacher and a method to check if you run into them
+	{
+		if( chance >= random.nextInt(100) + 1) //if statement to roll your chance
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+		
+	}
+	
+	public boolean encounterPuzzle(int chance) //chance to encounter a puzzle if you did not encounter a teacher
+	{
+		if( chance >= random.nextInt(100) + 1) //if statement to roll your chance
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
 	}
 	
 }
--- DiscoveringGGC/src/Teacher.java
+++ DiscoveringGGC/src/Teacher.java
@@ -8,5 +8,47 @@ public class Teacher
 	String teacherName; // this is the teachers name
 	int teacherID; //this is an identifier for the teacher
 	int spawnChance; //This is the percent chance that you will encounter this teacher
-	int[] questionIDBank; //this is an array of possible questions that the teacher may ask
+	int[] questionIDBank; //this is an array of possible questions that the teacher may ask 
+	
+	public void setTeacherName(String name) //A method to set the teachers name
+	{
+		teacherName = name;
+	}
+	
+	public void setTeacherID(int ID) //A method to set the teachers ID number
+	{
+		teacherID = ID;
+	}
+	
+	public void setSpawnChance(int chance) // a method to set the encounter chance of the teacher
+	{
+		spawnChance = chance;
+	}
+	
+	public void setQuestionBank(int[] questionlist) //A method which sets the question back that the teacher can use
+	{
+		questionIDBank = questionlist;
+	}
+	
+	public String getTeacherName(String name) //A method to get the teachers name
+	{
+		return teacherName;
+	}
+	
+	public int getTeacherID(int ID) //A method to get the teachers ID number
+	{
+		return teacherID;
+	}
+	
+	public int getSpawnChance(int chance) // a method to get the encounter chance of the teacher
+	{
+		return spawnChance;
+	}
+	
+	public int[] getQuestionBank(int[] questionlist) //A method which gets the question back that the teacher can use
+	{
+		return questionIDBank;
+	}
+	
+	
 }
