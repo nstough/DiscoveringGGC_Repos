4c1eae35637314fae103e9c7cc1e27e85f60f366
John Holcombe <jholcombe@ggc.edu>
2015-04-10 13:27:43 -0400

    Ryan plz

--- DiscoveringGGC/src/Room.java
+++ DiscoveringGGC/src/Room.java
@@ -8,6 +8,8 @@ public class Room
 	//Ryan was here
 	String roomDes; //This is the description of the room. The player will read it when he/she enters the room
 	int roomID; //This is the room id so that we know which room the player is in.
+	int[] puzzles; //This is a list of possible puzzles in this room
+	int[] teachers; // this is a list of possible teachers
 	int[] ajacentRooms; //This is an array of rooms that the player may enter from this room
 	
 	public String getRoomDescription()
